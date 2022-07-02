31b0c2db3839ee556c0e3406348b067ce614b51a
hell13t <jon-jei@mail.ru>
2022-07-03 00:37:50 +0600

    initial commit

---
+++ src/main/java/myPackage/Main.java
@@ -0,0 +1,42 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Write name: ");
+        String yourName = scan.nextLine();
+
+        URL url = new URL ("http://dummy.restapiexample.com/api/v1/create");
+
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        String jsonInputString = "{\"name\": \""+yourName+"\", \"salary\": \"123\", \"age\": \"23\"}";
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response);
+        }
+
+    }
+}
